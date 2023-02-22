0ff243c16a5577cc3364d62409a90f0a93a14b91
finne <finne@Finn>
2023-02-22 20:01:51 +0100

    Login und Logout / Variable
    Login überarbeitet sowie Logoutdaten hinzugefügt und Variablen Namen
    überarbeitet

--- LogReaderArk/src/Scanner/ProgrammStart.java
+++ LogReaderArk/src/Scanner/ProgrammStart.java
@@ -11,26 +11,33 @@ public class ProgrammStart {
 		File file = new File("C:\\Users\\finne\\git\\ArkLogReader\\ServerGame.log");
 		Scanner scan = new Scanner(file);
 		Scanner scan2 = new Scanner(file);
+		Scanner scan3 = new Scanner(file);
 		StringAnalyser timestamp = new StringAnalyser();
 		
 		String Output = "";
 		String Zeit = "";
-		String User = "";
+		String userLogin = "";
+		String userLogout = "";
 		
 		while(scan.hasNextLine()) {
 			Zeit = timestamp.Zeitstempel(scan.nextLine());
-			User = timestamp.UserLogin(scan2.nextLine());
+			userLogin = timestamp.UserLogin(scan2.nextLine());
+			userLogout = timestamp.UserLogout(scan3.nextLine());
 			
-			if(Zeit.equals("") || User.equals("")) {
-				
-			}else{
-				Output = User;
+			if(!userLogin.equals("")) {
+				Output = userLogin;
+				System.out.println(Output);
+			}
+			
+			if(!userLogout.equals("")) {
+				Output = userLogout;
 				System.out.println(Output);
 			}
 		}
 		
 		scan.close();
 		scan2.close();
+		scan3.close();
 	}
 
 }
--- LogReaderArk/src/Scanner/StringAnalyser.java
+++ LogReaderArk/src/Scanner/StringAnalyser.java
@@ -5,156 +5,156 @@ public class StringAnalyser {
 	public String Zeitstempel(String Line) {
 
 		
-		String Timestamp = "";
+		String timeStamp = "";
 		
 		char a = ' ';	
 		
-		int Length;
+		int length;
 		int mode = 0;
-		int Zustand = 0;
+		int zustand = 0;
 		
-		Length = Line.length();
+		length = Line.length();
 		
-		for(int i = 0; i<Length; i++) {
+		for(int i = 0; i<length; i++) {
 			
 			a = Line.charAt(i);
 			
 			switch(mode) {
 				//---------------------------------------------------------------
-				//Hier wird der Timestamp aus dem String gefiltert und bearbeitet
+				//Hier wird der timeStamp aus dem String gefiltert und bearbeitet
 				//---------------------------------------------------------------
 			case 0:
 					//---------------------------------------------------------------------
-					// Über die If Abfragen wird der Timestamp ins richtige Format geändert
+					// Über die If Abfragen wird der timeStamp ins richtige Format geändert
 					//---------------------------------------------------------------------
-				if (a == '[' & Zustand == 0) {
-					Zustand = 1;
+				if (a == '[' & zustand == 0) {
+					zustand = 1;
 					
-				}else if(a == '.' & Zustand == 1) {
+				}else if(a == '.' & zustand == 1) {
 						//--------------------------------------------------
 						// ersetzen der Punkte im Datum durch Spiegelstriche
 						//--------------------------------------------------
-					Timestamp = Timestamp + a;
-					Timestamp = Timestamp.replace('.','-');
+					timeStamp = timeStamp + a;
+					timeStamp = timeStamp.replace('.','-');
 					
 				}else if(a == '-') {
 						//----------------------------------------
 						// Übergang zwischen Datum- und Zeitangabe
 						//----------------------------------------
 					a = ' ';
-					Timestamp = Timestamp + a;
-					Zustand = 2;
-				}else if(a == '.' & Zustand == 2) {
+					timeStamp = timeStamp + a;
+					zustand = 2;
+				}else if(a == '.' & zustand == 2) {
 					
 						//---------------------------------------------------------
 						// Ersetzen der Punkte in der Zeitangabe durch Doppelpunkte
 						//---------------------------------------------------------
-					Timestamp = Timestamp + a;
-					Timestamp = Timestamp.replace('.',':');
+					timeStamp = timeStamp + a;
+					timeStamp = timeStamp.replace('.',':');
 					
 				}else if(a == ']') {
 					mode = 1;
 					
 				}else {
-					Timestamp = Timestamp + a;
+					timeStamp = timeStamp + a;
 				}
 			break;
 			
 			case 1:
-				return Timestamp;
+				return timeStamp;
 			}
 		}
-		return Timestamp;
+		return timeStamp;
 	}
 	
 	public String UserLogin(String Line) {
 		
-		String Result = "";
-		String TimStam = "";
-		String Num = "";
-		String Message = "";
-		String UserName = "";
-		String JoinMessage = "";
+		String result = "";
+		String timStam = "";
+		String num = "";
+		String message = "";
+		String userName = "";
+		String joinMessage = "";
 		
-		int Count = 0;
-		int Zustand = 0;
-		int LineLenght = 0;
-		int MessageLength = 0;
+		int count = 0;
+		int zustand = 0;
+		int lineLenght = 0;
+		int messagelength = 0;
 		int mode = 0;
 		
 		char a = ' ';
 		
-		LineLenght = Line.length();
+		lineLenght = Line.length();
 		
-		for(int i = 0; i<LineLenght; i++) {
+		for(int i = 0; i<lineLenght; i++) {
 			a = Line.charAt(i);
 			
 			switch(mode){
 			case 0:
 				if(a == ']') {
-					Count = 0;
+					count = 0;
 					mode = 1;
 				}
-				if(Count == 1) {
-					TimStam = TimStam + a;
+				if(count == 1) {
+					timStam = timStam + a;
 				}
 				if(a == '[') {
-					Count = 1;
+					count = 1;
 				}
 			break;
 			case 1:
 				if(a == ']') {
-					Count = 0;
+					count = 0;
 					mode = 2;
 				}
-				if(Count == 1) {
-					Num = Num + a;
+				if(count == 1) {
+					num = num + a;
 				}
 				if(a == '[') {
-					Count = 1;
+					count = 1;
 				}
 			break;
 			case 2:
-				Message = Message + a;
+				message = message + a;
 			break;
 			}
 			
 		}	
 		
-		MessageLength = Message.length();
+		messagelength = message.length();
 		mode = 0;
-		TimStam = "";
+		timStam = "";
 		
-		for(int i = 0; i<MessageLength; i++) {
-			a = Message.charAt(i);
+		for(int i = 0; i<messagelength; i++) {
+			a = message.charAt(i);
 			
 			switch(mode){
 			case 0:
 				if(a == ':') {
 					mode = 1;
 				}else {
-					TimStam = TimStam + a;
+					timStam = timStam + a;
 				}
 			break;
 			case 1:
-				if(a == ' ' && Zustand == 0) {
+				if(a == ' ' && zustand == 0) {
 					mode = 2;
-					Zustand = 1;
+					zustand = 1;
 				}
 			break;
 			case 2:
-				if(a == ' ' && Zustand == 1) {
+				if(a == ' ' && zustand == 1) {
 					mode = 3;
 				}else {
-					UserName = UserName + a;
+					userName = userName + a;
 				}
 			break;
 			case 3:
 				if(a == '!') {
-					JoinMessage = JoinMessage + a;
+					joinMessage = joinMessage + a;
 					mode = 4;
 				}else {
-					JoinMessage = JoinMessage + a;
+					joinMessage = joinMessage + a;
 				}
 			break;
 			default:
@@ -163,12 +163,118 @@ public class StringAnalyser {
 			}
 		}
 		
-		if(JoinMessage.equals("joined this ARK!")) {
-			Result = UserName + " " + TimStam + " " + JoinMessage;
-			return Result;
+		if(joinMessage.equals("joined this ARK!")) {
+			result = userName + " " + timStam + " " + joinMessage;
+			return result;
 		}else {
-			Result = "";
-			return Result;
+			result = "";
+			return result;
+		}
+		
+	}
+	
+public String UserLogout(String Line) {
+		
+		String result = "";
+		String timStam = "";
+		String num = "";
+		String message = "";
+		String userName = "";
+		String leaveMessage = "";
+		
+		int count = 0;
+		int zustand = 0;
+		int lineLenght = 0;
+		int messagelength = 0;
+		int mode = 0;
+		
+		char a = ' ';
+		
+		lineLenght = Line.length();
+		
+		for(int i = 0; i<lineLenght; i++) {
+			a = Line.charAt(i);
+			
+			switch(mode){
+			case 0:
+				if(a == ']') {
+					count = 0;
+					mode = 1;
+				}
+				if(count == 1) {
+					timStam = timStam + a;
+				}
+				if(a == '[') {
+					count = 1;
+				}
+			break;
+			case 1:
+				if(a == ']') {
+					count = 0;
+					mode = 2;
+				}
+				if(count == 1) {
+					num = num + a;
+				}
+				if(a == '[') {
+					count = 1;
+				}
+			break;
+			case 2:
+				message = message + a;
+			break;
+			}
+			
+		}	
+		
+		messagelength = message.length();
+		mode = 0;
+		timStam = "";
+		
+		for(int i = 0; i<messagelength; i++) {
+			a = message.charAt(i);
+			
+			switch(mode){
+			case 0:
+				if(a == ':') {
+					mode = 1;
+				}else {
+					timStam = timStam + a;
+				}
+			break;
+			case 1:
+				if(a == ' ' && zustand == 0) {
+					mode = 2;
+					zustand = 1;
+				}
+			break;
+			case 2:
+				if(a == ' ' && zustand == 1) {
+					mode = 3;
+				}else {
+					userName = userName + a;
+				}
+			break;
+			case 3:
+				if(a == '!') {
+					leaveMessage = leaveMessage + a;
+					mode = 4;
+				}else {
+					leaveMessage = leaveMessage + a;
+				}
+			break;
+			default:
+				
+			break;
+			}
+		}
+		
+		if(leaveMessage.equals("left this ARK!")) {
+			result = userName + " " + timStam + " " + leaveMessage;
+			return result;
+		}else {
+			result = "";
+			return result;
 		}
 		
 	}
